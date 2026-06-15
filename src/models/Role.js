45da180dc3afdafb8e1fcdc07679a69c3d478e1b
Sachin V S <vs1sachi12@gmail.com>
2026-06-15 17:30:08 +0530

    role-model

---
+++ src/models/Role.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const roleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      enum: ["SUPER_ADMIN", "COMPANY_ADMIN", "MANAGER", "USER"],
+    },
+    permissions: [
+      {
+        type: String,
+      },
+    ],
+    description: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Predefined roles and permissions
+const rolesData = {
+  SUPER_ADMIN: {
+    permissions: [
+      "manage_all_companies",
+      "manage_all_managers",
+      "manage_all_users",
+      "manage_all_tags",
+    ],
+  },
+  COMPANY_ADMIN: {
+    permissions: [
+      "manage_own_company",
+      "manage_managers_in_company",
+      "manage_users_in_company",
+      "manage_tags_in_company",
+    ],
+  },
+  MANAGER: {
+    permissions: [
+      "manage_assigned_users",
+      "create_tags_for_users",
+      "view_own_hierarchy_tags",
+    ],
+  },
+  USER: {
+    permissions: ["view_own_tags"],
+  },
+};
+
+roleSchema.statics.seedRoles = async function () {
+  for (const [roleName, roleData] of Object.entries(rolesData)) {
+    await this.findOneAndUpdate(
+      { name: roleName },
+      { name: roleName, ...roleData },
+      { upsert: true, new: true },
+    );
+  }
+  console.log("Roles seeded successfully");
+};
+
+module.exports = mongoose.model("Role", roleSchema);
