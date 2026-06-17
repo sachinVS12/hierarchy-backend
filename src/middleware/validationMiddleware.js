3234f3403dd7e23f976b4e34364b19c8985a711f
Sachin V S <vs1sachi12@gmail.com>
2026-06-17 17:12:32 +0530

    validation-middleware

---
+++ src/middleware/validationMiddleware.js
@@ -0,0 +1,20 @@
+const { validationResult } = require("express-validator");
+const AppError = require("../utils/appError");
+
+const validate = (validations) => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map((validation) => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    const extractedErrors = [];
+    errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
+
+    return next(new AppError("Validation failed", 400, extractedErrors));
+  };
+};
+
+module.exports = validate;
