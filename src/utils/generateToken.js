34f6f46c19cfa33f56c02ebeb3eb4bb21c51f7d6
Sachin V S <vs1sachi12@gmail.com>
2026-06-19 18:00:47 +0530

    utils generatetoken

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,3 +1,23 @@
-const mongoose = require("mongoose");
-const bcrypt = rquire("bcryptjs");
 const jwt = require("jsonwebtoken");
+const config = require("../config/env");
+
+const generateToken = (userId, role, companyId = null) => {
+  const payload = {
+    id: userId,
+    role,
+    companyId,
+  };
+
+  return jwt.sign(payload, config.jwtSecret, {
+    expiresIn: config.jwtExpire,
+  });
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, config.jwtSecret);
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
